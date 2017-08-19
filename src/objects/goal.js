48ab9ecbc00a92196fc4b610a242ab731dcb4604
David Goemans <dgoemans@gmail.com>
2017-08-19 14:20:19 +0200

    Fixed crash

--- src/objects/goal.js
+++ src/objects/goal.js
@@ -11,7 +11,7 @@ Goal.prototype.collide = function (other) {
     this.collides = false;
     // this.destroy();
     this.goalResource.add(1);
-    this.image.src = 'assets/wall_w.png';
+    this.image.src = 'assets/wall.png';
     if (this.onGoalReached) this.onGoalReached(this.x + this.width / 2, this.y + this.height / 2);
 };
 
